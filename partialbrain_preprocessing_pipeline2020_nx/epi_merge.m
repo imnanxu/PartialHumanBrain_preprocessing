7cd2f53abf4e66356a474bda8506e07122b8c973
Nan Xu <68754220+nxu25@users.noreply.github.com>
2021-10-07 15:43:09 -0400

    Add files via upload

---
+++ partialbrain_preprocessing_pipeline2020_nx/epi_merge.m
@@ -0,0 +1,18 @@
+%% ________Merge functional scans from different sessions____________ %
+function file_merge=epi_merge(fsldir, subjectdir, scans)
+
+
+k = strfind(subjectdir,'subject');
+sub = [subjectdir(k:end),' - ']; 
+cd(subjectdir);
+fprintf('--------Merge Functional data-----------\n\n')
+
+T = datetime('now'); time = whatsthetime(T);
+fprintf([time, sub, ': ','Merge all nii files ... '])
+cmd=[fsldir, 'fslmerge -t AllScans.nii.gz'];
+for scan_ct=1:length(scans)           
+    cmd=[cmd ' ' scans{scan_ct} '.nii'];        
+end
+system(cmd);  fprintf('Done\n')    
+file_merge='AllScans';
+
